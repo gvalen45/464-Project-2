72532105471d9a0e1b3fcabd2e095f493520180b
gvalen45 <61269234+gvalen45@users.noreply.github.com>
2023-11-03 18:05:34 -0700

    test

--- CSE 464 First Proj-2/src/main/java/GraphParser.java
+++ CSE 464 First Proj-2/src/main/java/GraphParser.java
@@ -7,7 +7,7 @@ import org.jgrapht.nio.dot.DOTImporter;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
+//test commit
 public class GraphParser {
     private final List<Integer> container = new ArrayList<>();
     private final Graph<String, DefaultEdge> graph;
